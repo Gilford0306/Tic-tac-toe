230e9075f685ed1bae1effb0483e6255e561c58a
Gilford0306 <50437968+Gilford0306@users.noreply.github.com>
2023-03-28 01:36:30 +0300

    3) Ячейка обучена реагировать на клик по себе (ставить Х или О )

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,49 +2,57 @@
 using System.Windows.Forms;
 
 namespace Tic_tac_toe
-{ partial class Form1
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
-    }
 
-    #region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Game";
-        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-        this.BackColor = Color.DeepSkyBlue;
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.BackColor = Color.DeepSkyBlue;
 
-        btn1 = new Button();
-        btn1.ForeColor = Color.Blue;
-        btn1.Size = new Size(90, 100);
-        btn1.Location = new Point(50, 50);
-        btn1.BackColor = Color.Yellow;
-        btn1.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn1 = new Button();
+            btn1.ForeColor = Color.Blue;
+            btn1.Size = new Size(90, 100);
+            btn1.Location = new Point(50, 50);
+            btn1.BackColor = Color.Yellow;
+            btn1.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn1.Click += (e, sender) =>
+            {
 
-        this.Controls.Add(btn1);
+                if (btn1.Text == "0" || btn1.Text == string.Empty)
+                    btn1.Text = "X";
+                else btn1.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn1.Enabled = false; };
+            };
+            this.Controls.Add(btn1);
             ///
 
             btn2 = new Button();
@@ -53,7 +61,14 @@ namespace Tic_tac_toe
             btn2.Location = new Point(150, 50);
             btn2.BackColor = Color.Yellow;
             btn2.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn2.Click += (e, sender) =>
+            {
 
+                if (btn2.Text == "0" || btn2.Text == string.Empty)
+                    btn2.Text = "X";
+                else btn2.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn2.Enabled = false; };
+            };
             this.Controls.Add(btn2);
             ///////
             btn3 = new Button();
@@ -62,7 +77,14 @@ namespace Tic_tac_toe
             btn3.Location = new Point(250, 50);
             btn3.BackColor = Color.Yellow;
             btn3.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
- 
+            btn3.Click += (e, sender) =>
+            {
+
+                if (btn3.Text == "0" || btn3.Text == string.Empty)
+                    btn3.Text = "X";
+                else btn3.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn3.Enabled = false; };
+            };
             this.Controls.Add(btn3);
 
             ////////
@@ -72,7 +94,14 @@ namespace Tic_tac_toe
             btn4.Location = new Point(50, 160);
             btn4.BackColor = Color.Yellow;
             btn4.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn4.Click += (e, sender) =>
+            {
 
+                if (btn4.Text == "0" || btn4.Text == string.Empty)
+                    btn4.Text = "X";
+                else btn4.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn4.Enabled = false; };
+            };
             this.Controls.Add(btn4);
             /////
 
@@ -82,7 +111,14 @@ namespace Tic_tac_toe
             btn5.Location = new Point(150, 160);
             btn5.BackColor = Color.Yellow;
             btn5.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn5.Click += (e, sender) =>
+            {
 
+                if (btn5.Text == "0" || btn5.Text == string.Empty)
+                    btn5.Text = "X";
+                else btn5.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn5.Enabled = false; };
+            };
             this.Controls.Add(btn5);
             ///////
             btn6 = new Button();
@@ -91,7 +127,14 @@ namespace Tic_tac_toe
             btn6.Location = new Point(250, 160);
             btn6.BackColor = Color.Yellow;
             btn6.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn6.Click += (e, sender) =>
+            {
 
+                if (btn6.Text == "0" || btn6.Text == string.Empty)
+                    btn6.Text = "X";
+                else btn6.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn6.Enabled = false; };
+            };
             this.Controls.Add(btn6);
             //    //////////
             btn7 = new Button();
@@ -100,7 +143,14 @@ namespace Tic_tac_toe
             btn7.Location = new Point(50, 270);
             btn7.BackColor = Color.Yellow;
             btn7.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn7.Click += (e, sender) =>
+            {
 
+                if (btn7.Text == "0" || btn7.Text == string.Empty)
+                    btn7.Text = "X";
+                else btn7.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn7.Enabled = false; };
+            };
             this.Controls.Add(btn7);
             /////
 
@@ -110,7 +160,14 @@ namespace Tic_tac_toe
             btn8.Location = new Point(150, 270);
             btn8.BackColor = Color.Yellow;
             btn8.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn8.Click += (e, sender) =>
+            {
 
+                if (btn8.Text == "0" || btn8.Text == string.Empty)
+                    btn8.Text = "X";
+                else btn8.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn8.Enabled = false; };
+            };
             this.Controls.Add(btn8);
             ///////
             btn9 = new Button();
@@ -119,7 +176,14 @@ namespace Tic_tac_toe
             btn9.Location = new Point(250, 270);
             btn9.BackColor = Color.Yellow;
             btn9.Font = new Font("Times New Roman", 23, FontStyle.Bold | FontStyle.Italic);
+            btn9.Click += (e, sender) =>
+            {
 
+                if (btn9.Text == "0" || btn9.Text == string.Empty)
+                    btn9.Text = "X";
+                else btn9.Text = "0";
+                btn_end.Click += (e1, sender1) => { btn9.Enabled = false; };
+            };
             this.Controls.Add(btn9);
 
 
